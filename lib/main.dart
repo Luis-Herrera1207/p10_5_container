c73e15b44d083eb0974e49967508f4e21b3efbed
LUIS_ALBERTO_HERRERA_GOMEZ <a.22308051281207@cbtis128.edu.mx>
2025-03-04 16:55:16 +0000

    adg

--- lib/main.dart
+++ lib/main.dart
@@ -16,53 +16,64 @@ class MyApp extends StatelessWidget {
           titleTextStyle: TextStyle(color: Color(0xff000000), fontSize: 20),
           backgroundColor: Color(0x005e3500),
         ),
-        body: Center(
-          child: iPhoneFrame(
-            // Envuelve el contenido en el marco de iPhone
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: MediaQuery.of(context).size.height * 0.7,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.black12),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.blue[100],
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                      ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'CONTAINER DECORATION',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Center(
-                      child: Container(
-                        padding: EdgeInsets.all(15),
-                        decoration: BoxDecoration(
-                          color: Colors.blue[200],
-                          borderRadius: BorderRadius.circular(20),
+        body: Container(
+          // Agrega un Container para el color de fondo
+          color: Colors.grey[200], // Cambia el color de fondo aquí
+          child: Center(
+            child: iPhoneFrame(
+              // Envuelve el contenido en el marco de iPhone
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.7,
+                decoration: BoxDecoration(
+                  color: Color(0xffd5d5d5),
+                  border: Border.all(color: Colors.black12),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: double.infinity,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.blue[100],
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
                         ),
+                      ),
+                      child: Center(
                         child: Text(
-                          'I am a container',
-                          style: TextStyle(fontSize: 18),
+                          'CONTAINER DECORATION',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                    Expanded(
+                      child: Center(
+                        child: Container(
+                          padding: EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                            color: Colors.blue[200],
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color(0xc300478b),
+                                blurRadius: 8,
+                                offset: Offset(2, 7),
+                              ),
+                            ],
+                          ),
+                          child: Text(
+                            'I am a container',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
